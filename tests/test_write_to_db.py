05916d40efb4d77a570f906f23a8ce989a12718f
Masum Billal <2643770+proafxin@users.noreply.github.com>
2022-09-03 02:33:45 +0600

    added test

--- tests/test_write_to_db.py
+++ tests/test_write_to_db.py
@@ -3,13 +3,14 @@
 import os
 
 import pymysql as pms
-from write_df.df_to_db import _get_mysql_connection
+from write_df.df_to_db import _get_column_info, _get_mysql_connection
 
 HOST = os.environ["MYSQL_HOST"]
 USER = os.environ["MYSQL_USER"]
 PASSWORD = os.environ["MYSQL_PASSWORD"]
 PORT = int(os.environ["MYSQL_PORT"])
 DBNAME = os.environ["MYSQL_DBNAME"]
+TABLE_NAMES = os.environ["TEST_TABLE_NAMES"].split(",")
 
 
 class TestWriteToMySQL:
@@ -26,3 +27,12 @@ class TestWriteToMySQL:
             port=PORT,
         )
         assert isinstance(conn, pms.connect)
+        for table_name in TABLE_NAMES:
+            print(table_name)
+            info = _get_column_info(
+                cursor=conn.cursor(), table_name=table_name.strip(), dbname=DBNAME
+            )
+            print(info.head())
+            print(info["COLUMN_NAME"].to_numpy())
+            print(info["IS_NULLABLE"].to_numpy())
+        assert 1 == 2
