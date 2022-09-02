3169b321e807c907257f44e739532fcd473b379c
Masum Billal <2643770+proafxin@users.noreply.github.com>
2022-09-03 02:34:09 +0600

    created connection from sqlalchemy

--- write_df/df_to_db.py
+++ write_df/df_to_db.py
@@ -1,15 +1,17 @@
 """Write a pandas dataframe to a database table"""
 
-from typing import Any
+
+from io import StringIO
 
 import pandas as pd
 import pymysql as pms
+from sqlalchemy import create_engine
 
 __all__ = ["write_df_to_db"]
 
 
 def _get_mysql_connection(host: str, dbname: str, user: str, password: str, port: int):
-    """_summary_
+    """Get MySQL connection from credentials.
 
     :param host: Host of the database.
     :type host: str
@@ -36,22 +38,83 @@ def _get_mysql_connection(host: str, dbname: str, user: str, password: str, port
     return connection
 
 
-def _get_column_info(cursor: Any, table_name: str, dbname: str):
-    query = f"SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`='{dbname}' AND `TABLE_NAME`='{table_name}';"
+def _get_column_info(cursor: pms.cursors.Cursor, table_name: str, dbname: str):
+
+    query = f"SELECT * FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`='{dbname}' AND `TABLE_NAME`='{table_name}';"
     cursor.execute(query)
+    cols = [detail[0] for detail in cursor.description]
     info = cursor.fetchall()
+    data = pd.DataFrame(info, columns=cols)
 
-    print(info)
+    return data
 
 
-def form_sql_query(data: pd.DataFrame, conn: Any, table_name: str):
+DIALECTS = {
+    "sqlserver": "mssql",
+    "mysql": "mysql",
+    "postgresql": "postgresql",
+}
+DRIVERS = {
+    "sqlserver": "pyodbc",
+    "mysql": "pyodbc",
+    "postgresql": "psycopg2",
+}
+
+
+def _get_sql_alchemy_engine(
+    dialect: str, host: str, user: str, password: str, dbname: str, port: int
+):
+
+    assert dialect in DIALECTS
+
+    driver = DRIVERS[dialect]
+    dialect = DIALECTS[dialect]
+
+    connection_string = f"{dialect}+{driver}://{user}:{password}@{host}:{port}/{dbname}"
+    engine = create_engine(connection_string, future=True)
+
+    return engine
+
+
+def _check_null(data: pd.DataFrame, info: pd.DataFrame):
+    columns = info["COLUMN_NAME"].to_numpy()
+    nullable_status = info["IS_NULLABLE"].to_numpy()
+
+    for column, status in zip(columns, nullable_status):
+        if status == "NO":
+            if data[column].dropna().shape[0] < data.shape[0]:
+                raise ValueError(f"`{column}` is non-nullable but has null value")
+
+    data = data[columns].copy()
+
+    return data
+
+
+def form_sql_query(data: pd.DataFrame, info: pd.DataFrame, table_name: str):
     pass
 
 
 def write_df_to_db(
-    data: pd.DataFrame, host: str, dbname: str, user: str, password: str, port: int, table_name: str
+    data: pd.DataFrame,
+    host: str,
+    dbname: str,
+    user: str,
+    password: str,
+    port: int,
+    table_name: str,
+    create_table: bool = False,
 ):
-    connection = _get_mysql_connection(
-        host=host, dbname=dbname, user=user, password=password, port=port
+    # connection = _get_mysql_connection(
+    #     host=host, dbname=dbname, user=user, password=password, port=port
+    # )
+    engine = _get_sql_alchemy_engine(
+        dialect="mysql", host=host, user=user, dbname=dbname, password=password, port=port
     )
-    _get_column_info(cursor=connection.cursor(), table_name=table_name, dbname=dbname)
+    connection = engine.connect()
+    cursor = connection.cursor()
+
+    _get_column_info(cursor=cursor, table_name=table_name, dbname=dbname)
+    # info = _get_column_info(cursor=connection.cursor(), table_name=table_name, dbname=dbname)
+    if create_table:
+        query = f"DROP TABLE IF EXISTS {table_name}"
+        cursor.execute(query)
