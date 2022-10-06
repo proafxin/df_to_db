b24891bc6a47023eb5b82009e267ce6869045e07
Masum Billal <2643770+proafxin@users.noreply.github.com>
2022-10-07 04:27:16 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="dataframe-to-database",
-    version="0.1.0",
+    name="df-to-db",
+    version="1.1",
     author="Masum Billal",
     author_email="billalmasum93@gmail.com",
     packages=find_packages(),
